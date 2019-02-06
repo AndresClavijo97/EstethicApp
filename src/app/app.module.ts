9c641b36c8d67072609c45016b3943be020e2175
Andres Clavijo <andress.clavijo96@gmail.com>
2019-02-06 10:08:36 -0500

    se quitaron las importaciones de list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,12 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-// import { ListPage } from '../pages/list/list';
 import { CreatePatientPage } from '../pages/create-patient/create-patient';
 import { EditPatientPage } from "../pages/edit-patient/edit-patient";
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { SQLite, SQLiteObject } from '@ionic-native/sqlite/ngx';
+import { SQLite } from '@ionic-native/sqlite/ngx';
 import { DataBaseSqliteProvider } from '../providers/data-base-sqlite/data-base-sqlite';
 
 @NgModule({
