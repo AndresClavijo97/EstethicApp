8ef64cedd0353664e469a8652fcd57a85eaaf921
Andres Clavijo <andress.clavijo96@gmail.com>
2019-02-06 09:50:25 -0500

    se agrego el provider dataBaseSqlite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { CreatePatientPage } from '../pages/create-patient/create-patient';
+import { EditPatientPage } from '../pages/edit-patient/edit-patient';
+
 
 @Component({
   templateUrl: 'app.html'
@@ -22,7 +25,10 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'List', component: ListPage },
+      { title: 'Crear Paciente', component: CreatePatientPage},
+      { title: 'Editar Datos Paciente', component: EditPatientPage}
+
     ];
 
   }
