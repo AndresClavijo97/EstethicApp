76eddd764be5e84d0d32d0e46536529feedf1c7b
Andres Clavijo <andress.clavijo96@gmail.com>
2019-02-06 15:51:47 -0500

    se valido la funcionalidad del searchbar

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,7 +7,7 @@ import { CreatePatientPage } from "../create-patient/create-patient";
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  patients: any = [{first_name: 'Nombre del paciente', phone_number: 'Telefono celular'}];
   constructor(public navCtrl: NavController) {
 
   }
@@ -16,5 +16,17 @@ export class HomePage {
     this.navCtrl.push(CreatePatientPage);
   }
 
+  getPatients($event){
+    const valor = $event.target.value;
+    console.log('valor del control' + valor);
+  }
+
+  edit(item){
+
+  }
+
+  destroy(item){
+
+  }
 
 }
