2bb3930d5869b035e9cb223a157b3748751acfdc
Andres Clavijo <andress.clavijo96@gmail.com>
2019-02-06 11:43:24 -0500

    se agrego la navegacion a createPatientPage y un ejemplo basico de formulario

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { CreatePatientPage } from "../create-patient/create-patient";
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,10 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  goToCreatePatientPage(){
+    this.navCtrl.push(CreatePatientPage);
   }
 
 }
